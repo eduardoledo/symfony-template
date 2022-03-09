9480151bbdc50aef9ddd8288664fb14e60adf5c2
Eduardo Ledo <eduardo.ledo@gmail.com>
2022-03-08 23:55:11 -0300

    Added bootstrap5 and fontawesome as node dependencies

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,15 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './bootstrap5/bootstrap.css';
-import './chosenjs/chosen.min.css';
+const $ = require('jquery');
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+//import './chosenjs/chosen.min.css';
+
 
 // start the Stimulus application
-import './bootstrap';
+require('bootstrap');
+
 import './main';
 import './chosenjs/chosen.jquery.min';
